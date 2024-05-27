44ef8a51cf65cd065888ff5bd238bf9f613029f4
krunaldevtale <krunaldevtale79@gmail.com>
2024-05-27 09:44:05 +0530

    image optimization

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,7 +52,13 @@ export default function Login() {
       >
         <div className="w-full max-w-xl  bg-gray-200 bg-opacity-50 h-2/3 md:h-1/2 lg:h-3/4 xl:h-2/3 rounded-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="w-full h-24 flex items-center justify-center">
-            <Image src="/images/logo.png" alt="Logo" width={70} height={100} />
+            <Image
+              src="/images/logo.png"
+              alt="logo"
+              width={50}
+              height={40}
+              style={{ width: "70px", height: "auto" }}
+            />
           </div>
           <div className="flex min-h-min">
             <div className="relative w-2/5 flex items-center justify-center">
@@ -60,15 +66,17 @@ export default function Login() {
                 className="absolute z-0"
                 src="/images/Goldbg.png"
                 alt="GoldBackground Image"
-                width={200}
-                height={200}
+                width={100}
+                height={100}
+                style={{ width: "200px", height: "200px" }}
               />
               <Image
                 className="z-10"
                 src="/images/profile.png"
                 alt="Profile"
-                width={100}
-                height={100}
+                width={50}
+                height={40}
+                style={{ width: "100px ", height: "100px" }}
               />
             </div>
             <div className="w-3/5">
